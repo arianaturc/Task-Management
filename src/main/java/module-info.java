c782111cc164f31abd3287f35aeb14da00c7fa4d
arianaturc <ariana.turc@yahoo.com>
2025-03-09 16:45:13 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.pt2025_30422_turc_ariana_assignment_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.pt2025_30422_turc_ariana_assignment_1 to javafx.fxml;
+    exports org.example.pt2025_30422_turc_ariana_assignment_1;
+}
---
+++ src/main/java/org/example/pt2025_30422_turc_ariana_assignment_1/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.pt2025_30422_turc_ariana_assignment_1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
